727bf518c5f03624e9e8422c10a2eaff56f41c3a
tzpranto <md.toufikzaman@gmail.com>
2019-07-06 23:40:25 +0600

    Email Verification Added
    By default login in takes to home. Did this for testing purpose. change to project:index for your convenience.

--- CRISPR_Suite/settings.py
+++ CRISPR_Suite/settings.py
@@ -131,3 +131,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = "bootstrap3"
 THUMBNAIL_EXTENSION = "png"
+
+#EMAIL confirmation
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS=True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='crisprsuite@gmail.com'
+EMAIL_HOST_PASSWORD='crisprsuite123'
+EMAIL_PORT=587
