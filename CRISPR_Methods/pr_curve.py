ea188d4f721b4822d517742cda78712586edcb80
Rafid013 <1405013.ahmr@ugrad.cse.buet.ac.bd>
2019-07-24 02:01:50 +0600

    COMPARISON DONE

--- CRISPR_Methods/pr_curve.py
+++ CRISPR_Methods/pr_curve.py
@@ -17,3 +17,21 @@ def draw_pr_curve(y_true, y_pred):
     plt.xlabel('Recall')
     plt.legend(loc='lower left', fancybox=True, shadow=True, ncol=1)
     return plt
+
+
+def draw_multiple_pr_curve(y_true, y_pred_list, labels):
+    plt.figure(figsize=(6, 5))
+    plt.title('Precision Recall Curve')
+
+    for y_pred, label in zip(y_pred_list, labels):
+        p, r, threshold = metrics.precision_recall_curve(y_true, y_pred)
+        pr_auc = metrics.average_precision_score(y_true, y_pred)
+        plt.plot(r, p, label=label + ' PR AUC = %0.2f' % pr_auc)
+        plt.legend(loc='lower right')
+
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('Precision')
+    plt.xlabel('Recall')
+    plt.legend(loc='lower left', fancybox=True, shadow=True, ncol=1)
+    return plt
--- CRISPR_Methods/roc_curve.py
+++ CRISPR_Methods/roc_curve.py
@@ -18,3 +18,22 @@ def draw_roc_curve(y_true, y_score):
     plt.xlabel('False Positive Rate')
     plt.legend(loc='lower right', fancybox=True, shadow=True, ncol=1)
     return plt
+
+
+def draw_multiple_roc_curve(y_true, y_score_list, labels):
+    plt.figure(figsize=(6, 5))
+    plt.title('Receiver Operating Characteristic Curve')
+
+    for y_score, label in zip(y_score_list, labels):
+        fpr, tpr, threshold = metrics.roc_curve(y_true, y_score)
+        roc_auc = metrics.auc(fpr, tpr)
+        plt.plot(fpr, tpr, label=label + ' AUC = %0.2f' % roc_auc)
+        plt.legend(loc='lower right')
+
+    plt.plot([0, 1], [0, 1], '--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.legend(loc='lower right', fancybox=True, shadow=True, ncol=1)
+    return plt
