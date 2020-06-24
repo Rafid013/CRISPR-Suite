7e6275398dd2b088bd411b4a685501fd25919cb5
Rafid013 <1405013.ahmr@ugrad.cse.buet.ac.bd>
2020-06-24 12:49:12 +0600

    ###

--- CRISPR_Suite/settings.py
+++ CRISPR_Suite/settings.py
@@ -139,3 +139,10 @@ CRISPY_TEMPLATE_PACK = "bootstrap3"
 THUMBNAIL_EXTENSION = "png"
 # EMAIL confirmation
 # Gmail Setup
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'crispr@grad.cse.buet.ac.bd'
+EMAIL_USER_NAME = 'crispr@grad.cse.buet.ac.bd'
+EMAIL_HOST_PASSWORD = 'genomePrediction2020'
+EMAIL_PORT = 587
